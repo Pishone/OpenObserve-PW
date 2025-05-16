3cbd97296775b85010426a518160872afaac1bb7
Pishone <pishone@accuknox.com>
2025-05-16 13:10:49 +0530

    fix: env variables

--- global-setup.js
+++ global-setup.js
@@ -7,8 +7,8 @@ export default async function globalSetup() {
   const browser = await chromium.launch();
   const page = await browser.newPage();
 
-  await page.goto(`${process.env.BASEURL}/web/login`);
-  await page.fill('[data-test="login-user-id"]', process.env.USERNAME);
+  await page.goto(`${process.env.BASE_URL}/web/login`);
+  await page.fill('[data-test="login-user-id"]', process.env.USER_NAME);
   await page.fill('[data-test="login-password"]', process.env.PASSWORD);
   await page.getByRole('button', { name: 'Login' }).click();
 
